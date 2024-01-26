41c58104c03a2288d4c4da955c745e727dc718c1
Gourav Dutta <108947265+GouravDutta-01@users.noreply.github.com>
2024-01-27 00:11:30 +0530

    Add mobile routing in hamburger menu

--- frontend/src/components/HamburgerMenu.jsx
+++ frontend/src/components/HamburgerMenu.jsx
@@ -1,49 +1,98 @@
-import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText} from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu';
-import React, { useState } from 'react'
+import React, { useContext, useState } from "react";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../context/Context";
 
 export default function MobileMenu() {
-    const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(Context);
+
+  const handleLogout = () => {
+    setOpenMenu(false);
+    dispatch({ type: "LOGOUT" });
+    navigate("/login");
+  };
+
+  const handleMenuClick = (route) => {
+    setOpenMenu(false);
+    navigate(route);
+  };
+
   return (
     <>
-        <Drawer open={openMenu}
-        onClose={()=>setOpenMenu(false)}>
-            <List>
-                <ListItemButton onClick={()=>setOpenMenu(false)}>
-                    <ListItemIcon>
-                        <ListItemText>HOME</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ListItemText>ABOUT</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ListItemText>CONTACT</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ListItemText>WRITE</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ListItemText>LOGIN</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <ListItemText>REGISTER</ListItemText>
-                    </ListItemIcon>
-                </ListItemButton>
-            </List>
-        </Drawer>
-        <IconButton sx={{color: "white", marginLeft: "auto"}} onClick={()=>setOpenMenu(!openMenu)}>
-            <MenuIcon/>
-        </IconButton>
+      <Drawer
+        anchor="right"
+        PaperProps={{
+          sx: { width: "50%" },
+        }}
+        open={openMenu}
+        onClose={() => setOpenMenu(false)}
+      >
+        <List>
+          <ListItemButton onClick={() => handleMenuClick("/")}>
+            <ListItemIcon>
+              <ListItemText>Home</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton onClick={() => handleMenuClick("/about")}>
+            <ListItemIcon>
+              <ListItemText>About</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton onClick={() => handleMenuClick("/contact")}>
+            <ListItemIcon>
+              <ListItemText>Contact</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton onClick={() => handleMenuClick("/write")}>
+            <ListItemIcon>
+              <ListItemText>Write</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
+          {user ? (
+            <>
+              <ListItemButton onClick={() => handleMenuClick("/settings")}>
+                <ListItemIcon>
+                  <ListItemText>Settings</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+              <ListItemButton onClick={handleLogout}>
+                <ListItemIcon>
+                  <ListItemText>Logout</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+            </>
+          ) : (
+            <>
+              <ListItemButton onClick={() => handleMenuClick("/login")}>
+                <ListItemIcon>
+                  <ListItemText>Login</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+              <ListItemButton onClick={() => handleMenuClick("/register")}>
+                <ListItemIcon>
+                  <ListItemText>Register</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+            </>
+          )}
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ color: "white", marginLeft: "auto" }}
+        onClick={() => setOpenMenu(!openMenu)}
+      >
+        <MenuIcon />
+      </IconButton>
     </>
-  )
+  );
 }
