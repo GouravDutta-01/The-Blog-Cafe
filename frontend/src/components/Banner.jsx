7271332ce4253fd7ddc2440a21fd27546c222912
Gourav Dutta <108947265+GouravDutta-01@users.noreply.github.com>
2024-02-17 00:06:10 +0530

    Update Banner.jsx

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,20 +1,112 @@
+import React, { useState } from "react";
 import styled from '@emotion/styled'
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, IconButton, Typography } from "@mui/material";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ArrowBack from "@mui/icons-material/ArrowBack";
+import ArrowForward from "@mui/icons-material/ArrowForward";
+
 const Image = styled(Box)({
-    backgroundImage: 'url("https://img.freepik.com/free-photo/top-view-arrangement-natural-material-stationery_23-2148898233.jpg?size=626&ext=jpg&ga=GA1.1.1724996770.1704636024&semt=ais")',
-    backgroundPosition: 'center',
-    color: "#27968b",
-    height: "50vh", 
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-})
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  color: "white",
+  height: "50vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
 
 export default function Banner() {
+  const images = [
+    "https://images.unsplash.com/photo-1620570623737-efc0ec4ab486?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.pexels.com/photos/2908984/pexels-photo-2908984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+
+  const texts = [
+    "Craft Your Voice: Start Writing, Start Exploring!",
+    "Your Words, Your Community: Create and Discover Blogs with Ease.",
+    "From Stories to Sagas: Join the Journey of Writing and Reading.",
+  ];
+
+  const [currentText, setCurrentText] = useState(texts[0]);
+
+  const CustomPrevArrow = ({ onClick }) => (
+    <IconButton
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: 15,
+        zIndex: 1,
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+      }}
+      onClick={onClick}
+    >
+      <ArrowBack />
+    </IconButton>
+  );
+
+  const CustomNextArrow = ({ onClick }) => (
+    <IconButton
+      style={{
+        position: "absolute",
+        top: "50%",
+        right: 15,
+        zIndex: 1,
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+      }}
+      onClick={onClick}
+    >
+      <ArrowForward />
+    </IconButton>
+  );
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    beforeChange: (current, next) => {
+      setCurrentText(texts[next]);
+    },
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
+
   return (
-    <Image>
-        <Typography variant="h2" fontWeight= "600" fontFamily= "cursive">TheBlogCafe</Typography>
-    </Image>
-  )
+    <div style={{ overflow: "hidden" }}>
+      <Slider {...settings} css={{ width: "100%", overflow: "hidden" }}>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            css={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Image style={{ backgroundImage: `url('${image}')` }}>
+              <Typography
+                variant="h4"
+                fontWeight="600"
+                fontFamily="cursive"
+                textAlign="center"
+                style={{
+                  fontFamily: "Georgia, serif",
+                  paddingLeft: "20px",
+                  paddingRight: "20px",
+                }}
+              >
+                {currentText}
+              </Typography>
+            </Image>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
