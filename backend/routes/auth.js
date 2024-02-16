da8cf032686616ed11f0cf56d89e12097f07ca00
Gourav Dutta <108947265+GouravDutta-01@users.noreply.github.com>
2024-02-17 00:25:51 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -22,14 +22,18 @@ router.post("/register", async (req, res)=>{
 })
 //Login
 router.post("/login", async (req, res)=>{
-    try{
-        const user = await User.findOne({username: req.body.username});
-        !user && res.status(400).json("Wrong credentials !");
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            return res.status(400).json("Wrong credentials!");
+        }
         const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("Wrong credentials !");
-        const {password, ...others} = user._doc;
+        if (!validated) {
+            return res.status(400).json("Wrong credentials!");
+        }
+        const { password, ...others } = user._doc;
         res.status(200).json(others);
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
